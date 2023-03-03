61aba39bc991555cac232ae50e5c119b14743e60
venkatesh780 <venkatesh131780@gmail.com>
2023-03-03 19:40:51 +0530

    Created Snake Board

---
+++ snake.js
@@ -0,0 +1,20 @@
+//board
+var blockSize = 25;
+var rows = 20;
+var cols = 20;
+var board;
+var context;
+
+window.onload = function () {
+  board = document.getElementById("board");
+  board.height = rows * blockSize;
+  board.width = cols * blockSize;
+  context = board.getContext("2d");
+
+  update();
+};
+
+function update() {
+  context.fillStyle = "black";
+  context.fillRect(0, 0, board.width, board.height);
+}
