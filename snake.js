22959ba38a6e7d9c60e6caca5c078ea3bfce3ca8
venkatesh780 <venkatesh131780@gmail.com>
2023-03-03 20:12:44 +0530

    Added snake and snake food

--- snake.js
+++ snake.js
@@ -5,16 +5,36 @@ var cols = 20;
 var board;
 var context;
 
+// snake head
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+// snake food
+var foodX;
+var foodY;
+
 window.onload = function () {
   board = document.getElementById("board");
   board.height = rows * blockSize;
   board.width = cols * blockSize;
   context = board.getContext("2d");
 
+  placeFood();
   update();
 };
 
 function update() {
   context.fillStyle = "black";
   context.fillRect(0, 0, board.width, board.height);
+
+  context.fillStyle = "lime";
+  context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+  context.fillStyle = "red";
+  context.fillRect(foodX, foodY, blockSize, blockSize);
+}
+
+function placeFood() {
+  foodX = Math.floor(Math.random() * cols) * blockSize;
+  foodY = Math.floor(Math.random() * rows) * blockSize;
 }
